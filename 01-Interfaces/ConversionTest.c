93c2cc3a0192c7ce4d328c406ac754edce093fd2
fjara <fjaralampidis@gmail.com>
2022-05-30 02:30:37 -0300

    Actualize los tests de ConversionTest.c

--- 01-Interfaces/ConversionTest.c
+++ 01-Interfaces/ConversionTest.c
@@ -1,23 +1,17 @@
 #include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #include "Conversion.h"
 
 int main() {
-  // Tests Farenheit
-  const int delta = 0.0000000001;
-  const double resultadoCelsius = abs(Celsius(40) - 4.444444444444444);
-  const double resultadoFarenheit = abs(Farenheit(1.54321) - 34.777778);
+  const double delta = 0.0000000000001;
+  const double resultadoCelsius = Celsius(40) - 4.444444444444444;
+  const double resultadoFarenheit = Farenheit(1.54321) - 34.777778;
 
-  printf("%lf", resultadoFarenheit);
+  assert(Celsius(32) == 0);
+  assert(resultadoCelsius < delta);
 
-  // assert(Celsius(32) == 0);
-  // assert(resultadoCelsius < delta);
-
-  // assert(Farenheit(0) == 32);
-  // assert(resultadoFarenheit < delta);
-  // PrintTablas(0, 25, 1);
+  assert(Farenheit(0) == 32);
+  assert(resultadoFarenheit < delta);
 
   return 0;
 }
