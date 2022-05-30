37ac8953a48dd0e37afc9c4bf9148a8ca4f95dc4
fjara <fjaralampidis@gmail.com>
2022-05-30 02:31:46 -0300

    Removi las funciones para mostrar tablas por consola

--- 01-Interfaces/Conversion.h
+++ 01-Interfaces/Conversion.h
@@ -6,16 +6,4 @@ double Celsius(double Farenheit);
 
 double Farenheit(double Celsius);
 
-void PrintTablas(int limiteInferior, int limiteSuperior, int incremento);
-
-void PrintTablaCelcius(int limiteInferior, int limiteSuperior, int incremento);
-
-void PrintTablaFarenheit(int limiteInferior, int limiteSuperior,
-                         int incremento);
-
-void PrintFilas(double (*conversion)(), int limiteInferior, int limiteSuperior,
-                int incremento);
-
-void PrintFila(double (*conversion)(), double temperatura);
-
 #endif  // CONVERSION
