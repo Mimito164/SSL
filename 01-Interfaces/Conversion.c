908730da48db2f2dabd578e8853d5e1a9e8cfb97
fjara <fjaralampidis@gmail.com>
2022-06-05 22:47:05 -0300

    removi la stdio.h ya que no estaba siendo utilizada.

--- 01-Interfaces/Conversion.c
+++ 01-Interfaces/Conversion.c
@@ -1,7 +1,5 @@
 #include "Conversion.h"
 
-#include <stdio.h>
-
 double Celsius(double Farenheit) { return 5 * (Farenheit - 32) / 9; }
 
 double Farenheit(double Celsius) { return (9 * Celsius / 5) + 32; }
